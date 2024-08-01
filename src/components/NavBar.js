524dd08dba33b7a3187ddd55b11abd90194541fe
Collins <collins.wachira@student.moringaschool.com>
2024-08-01 06:08:12 +0300

    new file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   return (
+    <div>
+      
     <nav>
       <a href="#home">I'm a link!</a>
+      
     </nav>
+    </div>
   );
 }
+
+export default NavBar;
